cf82a6c371f8800f2048af7adf761df84fe0d9d0
Elena <e.mandrovska@gmail.com>
2021-04-05 19:31:10 +0300

    task-7 done

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,14 @@
-// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
+// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) 
+// и изменяет инлайн - стиль span#text обновляя свойство font - size.
+// В результате при перетаскивании ползунка будет меняться размер текста.
+
+const refs = {
+    controller: document.querySelector('#font-size-control'),
+    text: document.querySelector('#text'),
+}
+function onControllerChangeSize() {
+    // refs.text.setAttribute('style', `font-size: ${refs.controller.value}px`);
+    refs.text.style.fontSize = `${refs.controller.value}px`
+}
+
+refs.controller.addEventListener('input', onControllerChangeSize);
